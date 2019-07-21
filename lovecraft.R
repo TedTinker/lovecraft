5281ebb58acd2b1ef579c594a96b6faa7635f7f3
tedjtinker <tedjtinker@gmail.com>
2019-07-21 13:58:06 -0700

    Added more setwd()

--- lovecraft.R
+++ lovecraft.R
@@ -1,4 +1,7 @@
 # Set to your working directory
 setwd("C:\\Users\\tedjt\\Desktop\\School\\2019 Summer\\FinalProject")
+#setwd("C:\\path\\to\\your\\repo")
+#setwd("C:\\path\\to\\your\\repo")
+#setwd("C:\\path\\to\\your\\repo")
 
 alchemist <- read.delim("alchemist.txt", header = TRUE)
