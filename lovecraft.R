74d5604d5929c36bc2466d0c717abb7a3ec1b447
tedjtinker <tedjtinker@gmail.com>
2019-07-21 15:47:44 -0700

    planning

--- lovecraft.R
+++ lovecraft.R
@@ -5,3 +5,12 @@ setwd("C:\\Users\\tedjt\\Desktop\\School\\2019 Summer\\FinalProject")
 #setwd("C:\\path\\to\\your\\repo")
 
 alchemist <- read.delim("alchemist.txt", header = TRUE)
+
+# What I'd like to do:
+
+# make a table that has each sentence of alchemist.txt in another row
+# count the number of words in each sentence
+# make graph of intenisty like https://www.youtube.com/watch?v=SaXdkb-hx6A
+# Do the same for every story. How do stories compare?
+
+# Then, make a frequency chart showing which words appear in which stories. 
